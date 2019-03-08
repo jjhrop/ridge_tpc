fe5c02fc1f9852d3b2f31e1472c4d0e766d50cec
jjhrop <44966706+jjhrop@users.noreply.github.com>
2019-03-08 14:41:58 +0200

    Update ridge_tpc.m

--- ridge_tpc.m
+++ ridge_tpc.m
@@ -18,7 +18,7 @@ function [b_lambda, b_lambda_opt, lambda_opt, sigma_b] = ridge_tpc(y, X, lambda,
     % into the sets by its data points. E.g. with K = 2 the two 
     % training sets are the first half and the second half.
     %
-    % cv_randomized: Signifies whether the data points are 
+    % cv_randomized: Signifies whether the order of the data points is
     % randomized for cross-validation. For instance, they are not
     % randomized when the data represents a time series, but randomization
     % is more suitable when the data points represent different test 
