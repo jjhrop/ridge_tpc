3c0a7ed42ed2aacb64fc3f4aea92b5707b98350f
jjhrop <44966706+jjhrop@users.noreply.github.com>
2019-03-08 14:41:39 +0200

    Update ridge_optimal_universal_parameter.m

--- ridge_optimal_universal_parameter.m
+++ ridge_optimal_universal_parameter.m
@@ -23,7 +23,7 @@ function [lambda_opt_universal, lambda_opt_list_sample, sample_indices] = ridge_
     % into the sets by its data points. E.g. with K = 2 the two 
     % training sets are the first half and the second half.
     %
-    % cv_randomized: Signifies whether the data points are 
+    % cv_randomized: Signifies whether the order of the data points is
     % randomized for cross-validation. For instance, they are not
     % randomized when the data represents a time series, but randomization
     % is more suitable when the data points represent different test 
